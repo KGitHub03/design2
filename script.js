159283abff93fc756f1478aae57f58bcbbd49315
KushM <kushmalwatkar@gmail.com>
2023-02-20 20:32:24 -0500

    dark mode

--- script.js
+++ script.js
@@ -8,10 +8,23 @@ function mode()
     {
         console.log("to dark mode");
         get("#link").href = "dark-style.css";
+        get(".dark-mode").innerHTML = "Light Mode";
     }
     else if(get("#link").href.slice(-14) == "dark-style.css")
     {
         console.log("to light mode");
         get("#link").href = "styles.css";
+        get(".dark-mode").innerHTML = "Dark Mode";
     }
 }
+function copyGCcode() {
+    // Get the text field
+    var copyText = get(".form-control-plaintext");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 7); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  }
